9e10623fff6b76dbd10e5a61239dbff4bca7a34f
hansleolml@gmail.com <hansleolml@gmail.com>
2024-11-12 16:26:59 -0500

    update

--- edithtml.sh
+++ edithtml.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# Esperar hasta que POD_NAMESPACE tenga un valor
 while [ -z "$SECRET_VALUE" ]; do
   echo "Esperando que la variable SECRET_VALUE esté disponible..."
   sleep 1
 done
 
-sed -i '/<span>server_url<\/span><\/p>/a <p class="smaller"><span>Environment2:<\/span> <span>'"$ENVIRONMENT"'<\/span><\/p>' /usr/share/nginx/html/index.html
-sed -i '/<span>server_url<\/span><\/p>/a <p class="smaller"><span>Secret value2:<\/span> <span>'"$SECRET_VALUE"'<\/span><\/p>' /usr/share/nginx/html/index.html
+sed -i '/<span>server_url<\/span><\/p>/a <p class="smaller"><span>Secret value:<\/span> <span>'"$SECRET_VALUE"'<\/span><\/p>' /usr/share/nginx/html/index.html
+sed -i '/<span>server_url<\/span><\/p>/a <p class="smaller"><span>Environment:<\/span> <span>'"$ENVIRONMENT"'<\/span><\/p>' /usr/share/nginx/html/index.html
 
-# Iniciar NGINX
 nginx -g "daemon off;"
