5d761862a919b20bd671540cac44e56eb2521f98
hansleolml@gmail.com <hansleolml@gmail.com>
2024-11-12 08:50:34 -0500

    hasta aca piola

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM nginxdemos/hello
 
 COPY edithtml.sh /edithtml.sh
+RUN chmod +x /edithtml.sh
+
 CMD ["/edithtml.sh"]
--- edithtml.sh
+++ edithtml.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-envsubst '${SECRET_VALUE}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html
+
+# Esperar hasta que POD_NAMESPACE tenga un valor
+while [ -z "$SECRET_VALUE" ]; do
+  echo "Esperando que la variable SECRET_VALUE esté disponible..."
+  sleep 1
+done
+
+# Usar `sed` para reemplazar el marcador de posición en el archivo HTML
+#sed -i "s/\${POD_NAMESPACE}/$POD_NAMESPACE/g" /usr/share/nginx/html/index.html
+sed -i '/<span>server_url<\/span><\/p>/a <p class="smaller"><span>Environment:<\/span> <span>'"$SECRET_VALUE"'<\/span><\/p>' /usr/share/nginx/html/index.html
+
+# Iniciar NGINX
 nginx -g "daemon off;"
